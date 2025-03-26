8e81c058190864ff6939af8f9e58e5c1b612cf78
Софія Щербатюк <sfchbtik@gmail.com>
2025-03-26 12:35:15 +0200

    Додада функцію яка записує відфільтровані рядки/текст у файл

--- textFiltration.py
+++ textFiltration.py
@@ -13,3 +13,11 @@ def filter_lines(lines: list, keyword: str) -> list:
     """Фільтрує рядки, що містять ключове слово."""
     return [line for line in lines if keyword in line]
 
+def write_to_file(output_file: str, lines: list):
+    """Записує відфільтровані рядки у новий файл."""
+    with open(output_file, "w", encoding="utf-8") as file:
+        file.writelines(lines)
+    print(f"Фільтрований файл збережено у {output_file}")
+
+
+
